be2cad091ba377f97388a8a6977847c16ca809f1
FranciscoEuroElettra <francisco.navarrete@nunsys.com>
2023-03-28 10:15:48 +0200

    No gutters row

--- src/components/cards/comun/Estado.vue
+++ src/components/cards/comun/Estado.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-row>
+  <v-row no-gutters>
     <v-col>
       <v-switch v-model="mostrar" color="info" label="Estado">Estado</v-switch>
       <v-card v-if="mostrar">
-        <v-row>
+        <v-row no-gutters>
           <v-col v-if="cargado">
             <ApexChart
               ref="chartRef"
--- src/components/cards/comun/FrutaProcesadaComun.vue
+++ src/components/cards/comun/FrutaProcesadaComun.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-row>
+  <v-row no-gutters>
     <v-col>
       <v-switch v-model="mostrar" color="info" label="Fruta procesada">
         Kg Fruta procesada
       </v-switch>
       <v-card v-if="mostrar">
-        <v-row>
+        <v-row no-gutters>
           <v-col v-if="cargado">
             <ApexChart
               ref="chartRef3"
