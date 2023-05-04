ceaec2cca88368c168a305b0fc74194097bbfe57
FranciscoEuroElettra <francisco.navarrete@nunsys.com>
2023-05-04 10:49:49 +0200

    Fix nodata

--- src/components/cards/comun/Estado.vue
+++ src/components/cards/comun/Estado.vue
@@ -195,7 +195,7 @@ async function dataGrafica(maquinaID) {
     maquinaID,
     routerStore().clienteID
   );
-  if (funcMaquina[1]) {
+  if (funcMaquina[1].data.length > 0) {
     for (let index = 0; index < funcMaquina[1].data.length; index++) {
       let element = funcMaquina[1].data[index];
       if (element.x == "Alarma") {
@@ -206,10 +206,13 @@ async function dataGrafica(maquinaID) {
       modoMaquina[1].data.push(element);
     }
   }
-  if (!modoMaquina) {
+  if (modoMaquina[1].data.length == 0) {
     cargado.value = false;
     noData.value = true;
-  } else series.value = modoMaquina;
+  } else {
+    series.value = modoMaquina;
+    cargado.value = true;
+  }
 }
 onMounted(async () => {
   cargado.value = false;
@@ -238,6 +241,5 @@ onMounted(async () => {
       dataGrafica(maquinaID);
     }
   );
-  cargado.value = true;
 });
 </script>
