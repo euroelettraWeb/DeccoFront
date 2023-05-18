42c5985f0fc517e57e52821cd941a5050537629c
FranciscoEuroElettra <francisco.navarrete@nunsys.com>
2023-05-18 12:25:33 +0200

    One page  and Lotes comp

---
+++ src/components/cards/comun/LoteCliente.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-row>
+    <v-col><v-subheader>Lote Cliente XXX</v-subheader></v-col>
+  </v-row>
+</template>
+<script setup>
+const props = defineProps({ tipo: { type: Number, default: 1 } });
+</script>
