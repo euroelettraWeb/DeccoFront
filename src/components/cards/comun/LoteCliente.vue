02a1ddf1899b8a81cf8b9525aab032642a35225e
euroelettraWeb <josefrancisco.lopez@nunsys.com>
2024-06-10 16:23:19 +0200

    git ignore LoteCliente

--- src/components/cards/comun/LoteCliente.vue
+++ src/components/cards/comun/LoteCliente.vue
@@ -1,56 +1,8 @@
 <template>
   <v-row>
-    <v-col v-if="cargado"
-      ><h1>{{ lotesCliente[0].x }}</h1></v-col
-    >
+    <v-col><h1>Lote Cliente XXX</h1></v-col>
   </v-row>
 </template>
 <script setup>
-import { onMounted, ref, onUnmounted, watch } from "vue";
-import { routerStore } from "../../../stores/index";
-import {
-  obtenerMaquina,
-  obtenerDatosVariableGeneral,
-} from "../../../helpers/bd";
-const props = defineProps({
-  lotecliente: { type: Number, default: null },
-  tipo: { type: Number, default: 1 },
-});
-const lotesCliente = ref([]);
-const cargado = ref(false);
-let interval = null;
-const cargarDatos = async () => {
-  let maquinaID = (
-    await obtenerMaquina("lineaTipo", routerStore().lineasID, props.tipo)
-  )[0].id;
-  lotesCliente.value = await obtenerDatosVariableGeneral(
-    "24H",
-    "registros",
-    "individual",
-    "formatoLotesCliente",
-    [props.lotecliente],
-    maquinaID,
-    routerStore().clienteID
-  );
-};
-watch(
-  () => routerStore().lineasID,
-  async () => {
-    cargado.value = false;
-    await cargarDatos();
-    cargado.value = true;
-  }
-);
-onMounted(async () => {
-  cargado.value = false;
-  await cargarDatos();
-  cargado.value = true;
-  interval = setInterval(() => {
-    cargarDatos();
-  }, 90000);
-});
-onUnmounted(() => {
-  clearInterval(interval);
-  lotesCliente.value = [];
-});
+const props = defineProps({ tipo: { type: Number, default: 1 } });
 </script>
